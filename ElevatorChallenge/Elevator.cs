9f33c25e11e95ca0ba3f87e42fb33318e3d35f6e
Treveshan Naidoo <treveshan.naidoo@gmail.com>
2022-10-04 23:49:18 +0200

    feat: basic pass at challenge

---
+++ ElevatorChallenge/Elevator.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ElevatorChallenge
+{
+    public class Elevator
+    {
+        private readonly int _maxFloor;
+        private readonly int _elevatorNumber;
+        private int _currentFloor;
+        private int _currentNumberOfPeople;
+        private ElevatorDirection _direction;
+        private bool[] _floorStatuses;
+        public Elevator(int maxFloor, int elevatorNumber, int startingFloor)
+        {
+            _elevatorNumber = elevatorNumber;
+            _maxFloor = maxFloor;
+            _currentFloor = startingFloor;
+            _floorStatuses = new bool[_maxFloor + 1];
+        }
+
+        private void Stop(int floor)
+        {
+            _direction = ElevatorDirection.Stopped;
+            _currentFloor = floor;
+            _floorStatuses[floor] = false;
+            Console.WriteLine("Stopped at floor {0}", floor);
+        }
+
+        private void Descend(int floor)
+        {
+            for (var i = _currentFloor; i >= 1; i--)
+            {
+                if (_floorStatuses[i])
+                    Stop(floor);
+                else
+                    continue;
+            }
+
+            _direction = ElevatorDirection.Stopped;
+            Console.WriteLine("Waiting..");
+        }
+
+        private void Ascend(int floor)
+        {
+            for (int i = _currentFloor; i <= _maxFloor; i++)
+            {
+                if (_floorStatuses[i])
+                    Stop(floor);
+                else
+                    continue;
+            }
+
+            _direction = ElevatorDirection.Stopped;
+            Console.WriteLine("Waiting..");
+        }
+
+        void FloorReached()
+        {
+            _currentNumberOfPeople = 0;
+            Console.WriteLine("Floor Reached");
+        }
+
+        public void FloorPress(int floor,int numberOfPeople)
+        {
+            _currentNumberOfPeople = numberOfPeople;
+            if (floor > _maxFloor)
+            {
+                Console.WriteLine("There are only {0} floors", _maxFloor);
+                return;
+            }
+
+            _floorStatuses[floor] = true;
+
+            if (_direction == ElevatorDirection.Down)
+            {
+                Descend(floor);
+            }
+            else if (_direction == ElevatorDirection.Stopped)
+            {
+                if (_currentFloor < floor)
+                    Ascend(floor);
+                else if (_currentFloor == floor)
+                    FloorReached();
+                else
+                    Descend(floor);
+            }
+            else if (_direction == ElevatorDirection.Up)
+            {
+                Ascend(floor);
+            }
+        }
+
+        public int GetCurrentFloor()
+        {
+            return _currentFloor; 
+        }
+
+        public bool Stopped()
+        {
+            return _direction == ElevatorDirection.Stopped; 
+        }
+
+        public void GetStatus()
+        {
+            Console.WriteLine($"Elevator: {_elevatorNumber}. Current Floor: {_currentFloor}. Direction: {_direction}. Number Of People: {_currentNumberOfPeople} ");
+        }
+    }
+
+    public enum ElevatorDirection
+    {
+        Stopped,
+        Up,
+        Down,
+    }
+}
---
+++ ElevatorChallenge/Program.cs
@@ -0,0 +1,68 @@
+﻿using ElevatorChallenge;
+
+Console.WriteLine("Hello!!!");
+
+var howManyFloorsInTheBuilding = "How many floors in the building?";
+Console.WriteLine(howManyFloorsInTheBuilding);
+var floorsInput = Console.ReadLine();
+var maxNumberOfFloors = GetNumberInput(floorsInput, howManyFloorsInTheBuilding);
+
+var howManyElevators = "How many elevators?";
+Console.WriteLine(howManyElevators);
+var elevatorsInput = Console.ReadLine();
+var maxNumberOfElevators = GetNumberInput(elevatorsInput, howManyElevators);
+
+var elevators = GenerateElevators(maxNumberOfElevators, maxNumberOfFloors);
+
+var stay = true;
+while (stay)
+{
+    var pressWhichFloorYouWouldLikeToGoTo = "Please enter the floor number";
+    Console.WriteLine(pressWhichFloorYouWouldLikeToGoTo);
+    var floorNumberInput = Console.ReadLine();
+    var floorNumber = GetNumberInput(floorNumberInput, pressWhichFloorYouWouldLikeToGoTo);
+
+    var pleaseEnterNumberOfPeople = "Please enter number of people";
+    Console.WriteLine(pleaseEnterNumberOfPeople);
+    var peopleInput = Console.ReadLine();
+    var peopleNumber = GetNumberInput(peopleInput, pleaseEnterNumberOfPeople);
+    var availableElevator = AvailableElevator(elevators, floorNumber);
+  
+    availableElevator.FloorPress(floorNumber, peopleNumber);
+
+    GetElevatorsStatus(elevators);
+}
+
+Console.ReadLine();
+
+int GetNumberInput(string? value, string retryMessage)
+{
+    var valid = int.TryParse(value, out var number);
+    while (!valid)
+    {
+        Console.WriteLine("Invalid Input. Try again...");
+        Console.WriteLine(retryMessage);
+        value = Console.ReadLine();
+        valid = int.TryParse(value, out number);
+    }
+
+    return number;
+}
+
+List<Elevator> GenerateElevators(int maxNumberOfElevators1, int maxNumberOfFloors1)
+{
+    var start = 1;
+    return Enumerable.Range(start, maxNumberOfElevators1).Select(i => new Elevator(maxNumberOfFloors1, i, start)).ToList();
+}
+
+void GetElevatorsStatus(List<Elevator> list)
+{
+    Console.WriteLine("-----------------Status-----------------");
+    list.ForEach(elevator => elevator.GetStatus());
+    Console.WriteLine("----------------------------------------");
+}
+
+Elevator? AvailableElevator(List<Elevator> elevators1, int floorNumber1)
+{
+    return elevators1.Where(elevator => elevator.Stopped()).OrderBy(elevator => floorNumber1 <= elevator.GetCurrentFloor()).FirstOrDefault();
+}
